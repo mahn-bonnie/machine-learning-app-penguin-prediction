3aa308dc26d422bb9058d8a5dbec5ceae454638b
Mahn Bonnie <mulibonface187@gmail.com>
2024-11-14 11:23:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.info('This is app builds a machine learning model!')
 
 with st.expander('Data'):
   st.write('**Raw data**')
-  df = pd.read_csv('/home/mahnbonnie/software/app/penguins_cleaned.csv')
+  df = pd.read_csv('https://raw.githubusercontent.com/mahn-bonnie/machine-learning-app-penguin-prediction/refs/heads/master/penguins_cleaned.csv')
   df
 
   st.write('**X**')
